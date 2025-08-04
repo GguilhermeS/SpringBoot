38a3634fa371c8b0819ccb98866bcb9a87d1c60b
GguilhermeS <guig75673@gmail.com>
2025-08-04 09:36:54 -0300

    Committwo

---
+++ src/main/java/com/br/ProjectTwo/DAO/IUsuario.java
@@ -0,0 +1,9 @@
+package com.br.ProjectTwo.DAO;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.br.ProjectTwo.model.Usuario;
+
+public interface IUsuario extends CrudRepository<Usuario, Integer>{
+
+}
